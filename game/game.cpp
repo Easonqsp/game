d8f502be959298f35efce890d1264e4752498d9b
Qian Yicheng <qsp2020@outlook.com>
2020-11-26 15:40:42 +0800

    first

--- game/game.cpp
+++ game/game.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿//main
+#include "main.h"
 using namespace std;
 int main()
 {
---
+++ game/main.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+const string ver = "0.1";
