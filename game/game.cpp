aadcbdfe26b41f5fb6b70ad3bb2cd1acba17f135
Qian Yicheng <qsp2020@outlook.com>
2020-12-09 21:18:09 +0800

    Add files via upload

---
+++ game/game.cpp
@@ -0,0 +1,16 @@
+﻿//main
+#include "main.h"
+using namespace std;
+int s=0;
+int main()
+{
+	cur(1);
+    cout << "Welcome to battle game ver: "<<ver<<ret;
+	pau();
+	cls();
+	cout << "Please choose the size of map ";
+	cin >> s;
+	cls();
+	pau();
+	return 0;
+}
---
+++ game/main.h
@@ -0,0 +1,6 @@
+#pragma once
+#include"includes.h"
+#include"console.h"
+#include"map.h"
+using namespace std;
+const string ver = "0.1";
